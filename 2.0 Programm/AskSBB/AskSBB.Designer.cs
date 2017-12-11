01a8c8730a69c423e6850434d79ae83d62d5a301
amolming <amol.ming@gmx.ch>
2017-12-11 01:59:26 +0100

    20171211_0200
    Running Program Update

--- 2.0 Programm/AskSBB/AskSBB.Designer.cs
+++ 2.0 Programm/AskSBB/AskSBB.Designer.cs
@@ -28,110 +28,189 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.VonLabel = new System.Windows.Forms.Label();
-            this.VonTextBox = new System.Windows.Forms.TextBox();
-            this.NachLabel = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.DatumLabel = new System.Windows.Forms.Label();
-            this.DatumTimePicker = new System.Windows.Forms.DateTimePicker();
-            this.ZeitLabel = new System.Windows.Forms.Label();
-            this.ZeitTextBox = new System.Windows.Forms.TextBox();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.fromLbl = new System.Windows.Forms.Label();
+            this.toLbl = new System.Windows.Forms.Label();
+            this.dateLbl = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.timeLbl = new System.Windows.Forms.Label();
+            this.timeTxtBox = new System.Windows.Forms.TextBox();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.fromTxtBox = new System.Windows.Forms.TextBox();
+            this.chooseLstBox = new System.Windows.Forms.ListBox();
+            this.toTxtBox = new System.Windows.Forms.TextBox();
+            this.resultsDGV = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.nearRdoBtn = new System.Windows.Forms.RadioButton();
+            this.connectionsRdoBtn = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.resultsDGV)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // VonLabel
+            // fromLbl
             // 
-            this.VonLabel.AutoSize = true;
-            this.VonLabel.Location = new System.Drawing.Point(31, 35);
-            this.VonLabel.Name = "VonLabel";
-            this.VonLabel.Size = new System.Drawing.Size(26, 13);
-            this.VonLabel.TabIndex = 0;
-            this.VonLabel.Text = "Von";
-            this.VonLabel.Click += new System.EventHandler(this.label1_Click);
+            this.fromLbl.AutoSize = true;
+            this.fromLbl.Location = new System.Drawing.Point(46, 55);
+            this.fromLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.fromLbl.Name = "fromLbl";
+            this.fromLbl.Size = new System.Drawing.Size(38, 20);
+            this.fromLbl.TabIndex = 0;
+            this.fromLbl.Text = "Von";
             // 
-            // VonTextBox
+            // toLbl
             // 
-            this.VonTextBox.Location = new System.Drawing.Point(89, 32);
-            this.VonTextBox.Name = "VonTextBox";
-            this.VonTextBox.Size = new System.Drawing.Size(200, 20);
-            this.VonTextBox.TabIndex = 1;
-            this.VonTextBox.TextChanged += new System.EventHandler(this.VonTextBox_TextChanged);
+            this.toLbl.AutoSize = true;
+            this.toLbl.Location = new System.Drawing.Point(46, 145);
+            this.toLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.toLbl.Name = "toLbl";
+            this.toLbl.Size = new System.Drawing.Size(46, 20);
+            this.toLbl.TabIndex = 2;
+            this.toLbl.Text = "Nach";
             // 
-            // NachLabel
+            // dateLbl
             // 
-            this.NachLabel.AutoSize = true;
-            this.NachLabel.Location = new System.Drawing.Point(381, 35);
-            this.NachLabel.Name = "NachLabel";
-            this.NachLabel.Size = new System.Drawing.Size(33, 13);
-            this.NachLabel.TabIndex = 2;
-            this.NachLabel.Text = "Nach";
+            this.dateLbl.AutoSize = true;
+            this.dateLbl.Location = new System.Drawing.Point(46, 240);
+            this.dateLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.dateLbl.Name = "dateLbl";
+            this.dateLbl.Size = new System.Drawing.Size(57, 20);
+            this.dateLbl.TabIndex = 4;
+            this.dateLbl.Text = "Datum";
             // 
-            // textBox1
+            // dateTimePicker
             // 
-            this.textBox1.Location = new System.Drawing.Point(458, 32);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(190, 20);
-            this.textBox1.TabIndex = 3;
+            this.dateTimePicker.Location = new System.Drawing.Point(134, 235);
+            this.dateTimePicker.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(298, 26);
+            this.dateTimePicker.TabIndex = 5;
             // 
-            // DatumLabel
+            // timeLbl
             // 
-            this.DatumLabel.AutoSize = true;
-            this.DatumLabel.Location = new System.Drawing.Point(31, 83);
-            this.DatumLabel.Name = "DatumLabel";
-            this.DatumLabel.Size = new System.Drawing.Size(38, 13);
-            this.DatumLabel.TabIndex = 4;
-            this.DatumLabel.Text = "Datum";
+            this.timeLbl.AutoSize = true;
+            this.timeLbl.Location = new System.Drawing.Point(46, 317);
+            this.timeLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.timeLbl.Name = "timeLbl";
+            this.timeLbl.Size = new System.Drawing.Size(36, 20);
+            this.timeLbl.TabIndex = 6;
+            this.timeLbl.Text = "Zeit";
             // 
-            // DatumTimePicker
+            // timeTxtBox
             // 
-            this.DatumTimePicker.Location = new System.Drawing.Point(89, 77);
-            this.DatumTimePicker.Name = "DatumTimePicker";
-            this.DatumTimePicker.Size = new System.Drawing.Size(200, 20);
-            this.DatumTimePicker.TabIndex = 5;
+            this.timeTxtBox.Location = new System.Drawing.Point(134, 317);
+            this.timeTxtBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.timeTxtBox.Name = "timeTxtBox";
+            this.timeTxtBox.Size = new System.Drawing.Size(84, 26);
+            this.timeTxtBox.TabIndex = 7;
+            this.timeTxtBox.Text = "HH:MM";
             // 
-            // ZeitLabel
+            // searchBtn
             // 
-            this.ZeitLabel.AutoSize = true;
-            this.ZeitLabel.Location = new System.Drawing.Point(381, 84);
-            this.ZeitLabel.Name = "ZeitLabel";
-            this.ZeitLabel.Size = new System.Drawing.Size(25, 13);
-            this.ZeitLabel.TabIndex = 6;
-            this.ZeitLabel.Text = "Zeit";
-            this.ZeitLabel.Click += new System.EventHandler(this.label1_Click_1);
+            this.searchBtn.Location = new System.Drawing.Point(801, 272);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(114, 43);
+            this.searchBtn.TabIndex = 9;
+            this.searchBtn.Text = "Suchen";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
             // 
-            // ZeitTextBox
+            // fromTxtBox
             // 
-            this.ZeitTextBox.Location = new System.Drawing.Point(458, 84);
-            this.ZeitTextBox.Name = "ZeitTextBox";
-            this.ZeitTextBox.Size = new System.Drawing.Size(190, 20);
-            this.ZeitTextBox.TabIndex = 7;
-            this.ZeitTextBox.Text = "HH:MM";
+            this.fromTxtBox.Location = new System.Drawing.Point(134, 55);
+            this.fromTxtBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.fromTxtBox.Name = "fromTxtBox";
+            this.fromTxtBox.Size = new System.Drawing.Size(298, 26);
+            this.fromTxtBox.TabIndex = 1;
+            this.fromTxtBox.TextChanged += new System.EventHandler(this.fromTxtBox_TextChanged);
             // 
-            // listView1
+            // chooseLstBox
             // 
-            this.listView1.Location = new System.Drawing.Point(89, 146);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(559, 197);
-            this.listView1.TabIndex = 8;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.chooseLstBox.FormattingEnabled = true;
+            this.chooseLstBox.ItemHeight = 20;
+            this.chooseLstBox.Location = new System.Drawing.Point(471, 55);
+            this.chooseLstBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.chooseLstBox.Name = "chooseLstBox";
+            this.chooseLstBox.Size = new System.Drawing.Size(300, 144);
+            this.chooseLstBox.TabIndex = 10;
+            this.chooseLstBox.Click += new System.EventHandler(this.chooseLstBox_Click);
+            // 
+            // toTxtBox
+            // 
+            this.toTxtBox.Location = new System.Drawing.Point(134, 145);
+            this.toTxtBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.toTxtBox.Name = "toTxtBox";
+            this.toTxtBox.Size = new System.Drawing.Size(298, 26);
+            this.toTxtBox.TabIndex = 11;
+            this.toTxtBox.TextChanged += new System.EventHandler(this.toTxtBox_TextChanged);
+            // 
+            // resultsDGV
+            // 
+            this.resultsDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.resultsDGV.Location = new System.Drawing.Point(50, 387);
+            this.resultsDGV.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.resultsDGV.Name = "resultsDGV";
+            this.resultsDGV.Size = new System.Drawing.Size(921, 303);
+            this.resultsDGV.TabIndex = 12;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.nearRdoBtn);
+            this.groupBox1.Controls.Add(this.connectionsRdoBtn);
+            this.groupBox1.Location = new System.Drawing.Point(801, 45);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(207, 154);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            // 
+            // nearRdoBtn
+            // 
+            this.nearRdoBtn.AutoSize = true;
+            this.nearRdoBtn.Location = new System.Drawing.Point(10, 68);
+            this.nearRdoBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.nearRdoBtn.Name = "nearRdoBtn";
+            this.nearRdoBtn.Size = new System.Drawing.Size(117, 24);
+            this.nearRdoBtn.TabIndex = 1;
+            this.nearRdoBtn.TabStop = true;
+            this.nearRdoBtn.Text = "In der Nähe";
+            this.nearRdoBtn.UseVisualStyleBackColor = true;
+            // 
+            // connectionsRdoBtn
+            // 
+            this.connectionsRdoBtn.AutoSize = true;
+            this.connectionsRdoBtn.Checked = true;
+            this.connectionsRdoBtn.Location = new System.Drawing.Point(10, 29);
+            this.connectionsRdoBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.connectionsRdoBtn.Name = "connectionsRdoBtn";
+            this.connectionsRdoBtn.Size = new System.Drawing.Size(134, 24);
+            this.connectionsRdoBtn.TabIndex = 0;
+            this.connectionsRdoBtn.TabStop = true;
+            this.connectionsRdoBtn.Text = "Verbindungen";
+            this.connectionsRdoBtn.UseVisualStyleBackColor = true;
             // 
             // AskSBB
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(721, 513);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.ZeitTextBox);
-            this.Controls.Add(this.ZeitLabel);
-            this.Controls.Add(this.DatumTimePicker);
-            this.Controls.Add(this.DatumLabel);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.NachLabel);
-            this.Controls.Add(this.VonTextBox);
-            this.Controls.Add(this.VonLabel);
+            this.ClientSize = new System.Drawing.Size(1026, 817);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.resultsDGV);
+            this.Controls.Add(this.toTxtBox);
+            this.Controls.Add(this.chooseLstBox);
+            this.Controls.Add(this.fromTxtBox);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.timeTxtBox);
+            this.Controls.Add(this.timeLbl);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.dateLbl);
+            this.Controls.Add(this.toLbl);
+            this.Controls.Add(this.fromLbl);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "AskSBB";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.resultsDGV)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -139,15 +218,20 @@
 
         #endregion
 
-        private System.Windows.Forms.Label VonLabel;
-        private System.Windows.Forms.TextBox VonTextBox;
-        private System.Windows.Forms.Label NachLabel;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label DatumLabel;
-        private System.Windows.Forms.DateTimePicker DatumTimePicker;
-        private System.Windows.Forms.Label ZeitLabel;
-        private System.Windows.Forms.TextBox ZeitTextBox;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label fromLbl;
+        private System.Windows.Forms.Label toLbl;
+        private System.Windows.Forms.Label dateLbl;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Label timeLbl;
+        private System.Windows.Forms.TextBox timeTxtBox;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.TextBox fromTxtBox;
+        private System.Windows.Forms.ListBox chooseLstBox;
+        private System.Windows.Forms.TextBox toTxtBox;
+        private System.Windows.Forms.DataGridView resultsDGV;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton nearRdoBtn;
+        private System.Windows.Forms.RadioButton connectionsRdoBtn;
     }
 }
 
--- 2.0 Programm/AskSBB/AskSBB.cs
+++ 2.0 Programm/AskSBB/AskSBB.cs
@@ -7,34 +7,152 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SwissTransport;
 
 namespace AskSBB
 {
     public partial class AskSBB : Form
     {
+        Transport transport = new Transport();
+        TextBox activeTxtBox = null;
+        string stationID = null;
+        string fixDestination = "Ebikon,St.Klemens";
+        
+
         public AskSBB()
         {
             InitializeComponent();
+            timeTxtBox.Text = DateTime.Now.ToShortTimeString();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void fromTxtBox_TextChanged(object sender, EventArgs e)
         {
-
+            activeTxtBox = fromTxtBox;
+            ShowStations(fromTxtBox.Text);
         }
 
-        private void label1_Click_1(object sender, EventArgs e)
+        private void toTxtBox_TextChanged(object sender, EventArgs e)
         {
-
+            activeTxtBox = toTxtBox;
+            ShowStations(toTxtBox.Text);
         }
 
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        private void chooseLstBox_Click(object sender, EventArgs e)
         {
-
+            activeTxtBox.Text = chooseLstBox.SelectedItem.ToString();
+            chooseLstBox.Items.Clear();
+            
         }
 
-        private void VonTextBox_TextChanged(object sender, EventArgs e)
+        private void searchBtn_Click(object sender, EventArgs e)
         {
+            if(connectionsRdoBtn.Checked == true)
+            {
+                ShowResults(fromTxtBox.Text, toTxtBox.Text, timeTxtBox.Text);
+            }
+            else if(nearRdoBtn.Checked == true)
+            {
+                ShowDepartures(fromTxtBox.Text);
+            }
+        }
+
+        private void ShowStations(string searchQuery)
+        {
+            Stations stationList = transport.GetStations(searchQuery);
+
+            chooseLstBox.Items.Clear();
+
+            foreach (Station s in stationList.StationList)
+            {
+                chooseLstBox.Items.Add(s.Name);
+            }
+        }
+
+        private void ShowResults(string source, string destination, string time)
+        {
+            Connections connectionList = transport.GetConnections(source, destination);
+
+            resultsDGV.Rows.Clear();
+            resultsDGV.Columns.Clear();
+
+            resultsDGV.AutoSize = true;
+
+            string[] dgv_columns = new string[] { "Von", "Nach", "Abfahrt", "Ankunft", "Gleis", "Dauer" };
+
+            foreach(string col in dgv_columns)
+            {
+                resultsDGV.Columns.Add(col, col);
+            }
+
+            foreach(Connection c in connectionList.ConnectionList)
+            {
+                int row = resultsDGV.Rows.Add();
+
+                string departure = c.From.Departure;
+                string arrival = c.To.Arrival;
+                string duration = c.Duration;
+
+                string[] conValues = new string[]
+                {
+                    c.From.Station.Name,
+                    c.To.Station.Name,
+                    departure,
+                    arrival,
+                    c.From.Platform,
+                    duration
+                };
+
+                for(int i = 0; i < conValues.Count(); i++)
+                {
+                    resultsDGV.Rows[row].Cells[i].Value = conValues[i];
+                }
+            }
+        }
+
+        private void ShowDepartures(string source)
+        {
+            Connections connectionList = transport.GetConnections(source, fixDestination);
+
+            foreach(Connection con in connectionList.ConnectionList)
+            {
+                stationID = con.From.Station.Id;
+            }
+
+            StationBoardRoot stationBoardList = transport.GetStationBoard(source, stationID);
+
+            resultsDGV.Rows.Clear();
+            resultsDGV.Columns.Clear();
+
+            resultsDGV.AutoSize = true;
+
+            string[] dgv_columns = new string[] { "Von", "Nach", "Abfahrt", "Ankunft", "Gleis", "Dauer" };
+
+            foreach (string col in dgv_columns)
+            {
+                resultsDGV.Columns.Add(col, col);
+            }
+
+            foreach(StationBoard stat in stationBoardList.Entries)
+            {
+                int row = resultsDGV.Rows.Add();
+
+                string[] statValues = new string[]
+                {
+                    stat.Name,
+                    stat.To,
+                    stat.Category,
+                    stat.Number,
+                    stat.Stop.Departure.ToString(),
+                    stat.Operator
+                };
+
+                for (int i = 0; i < statValues.Count(); i++)
+                {
+                    resultsDGV.Rows[row].Cells[i].Value = statValues[i];
+                }
+            }
 
         }
+
     }
 }
